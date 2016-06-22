1d772722d8072f43d4b783b509d7de72d3f08bee
Adrian Eguez <eadepto@hotmail.com>
2016-06-21 21:02:34 -0500

    Llamada al Servidor

--- Codigo/Chumas/assets/Aplicacion/Vistas/UsuariosCtrl.js
+++ Codigo/Chumas/assets/Aplicacion/Vistas/UsuariosCtrl.js
@@ -1,9 +1,30 @@
+aplicacion.controller("UsuariosCtrl", function ($scope, $http) {
+
+    $http({
+        method: "GET"
+        , url: "http://localhost:1337/Usuarios"
+        , isArray: true
+    }).then(
+        function (respuesta) {
+            
+            $scope.usuarios = respuesta.data;
+            
+            console.log(respuesta);
+        }
+        , function (error) {
+            console.log(error);
+        });
 
 
+    //    $http({method: $scope.method, url: $scope.url, cache: $templateCache}).
+    //        then(function(response) {
+    //          $scope.status = response.status;
+    //          $scope.data = response.data;
+    //        }, function(response) {
+    //          $scope.data = response.data || "Request failed";
+    //          $scope.status = response.status;
+    //      });
+    //    
 
 
-aplicacion.controller("UsuariosCtrl",function($scope){
-    
-    
-    
 });
