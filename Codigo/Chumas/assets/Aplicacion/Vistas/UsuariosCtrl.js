11e9d80fdae3d038953cc81eb64f9bd6e7e12af7
Adrian Eguez <eadepto@hotmail.com>
2016-06-21 22:01:53 -0500

    Anadir Nuevo Usuario
    12323

--- Codigo/Chumas/assets/Aplicacion/Vistas/UsuariosCtrl.js
+++ Codigo/Chumas/assets/Aplicacion/Vistas/UsuariosCtrl.js
@@ -1,30 +1,80 @@
 aplicacion.controller("UsuariosCtrl", function ($scope, $http) {
 
-    $http({
-        method: "GET"
-        , url: "http://localhost:1337/Usuarios"
-        , isArray: true
-    }).then(
-        function (respuesta) {
-            
-            $scope.usuarios = respuesta.data;
-            
-            console.log(respuesta);
+
+    $scope.nuevoUsuario = {
+        nombre: ""
+        , apellido: ""
+        , correo: ""
+        , password: ""
+        , fechaNac: ""
+    }
+    
+    $scope.botonCrearDeshabilitado = false;
+
+
+    $scope.crearUsuario = function () {
+        
+        $scope.botonCrearDeshabilitado = true;
+
+        $http({
+            method: "POST"
+            , url: "http://localhost:1337/Usuarios"
+            , data: $scope.nuevoUsuario
+        }).then(
+            function (respuesta) {
+                
+//                $scope.cargarUsuarios();
+                $scope.usuarios.push(respuesta.data);
+                $scope.resetearNuevoUsuario();
+                
+                $scope.botonCrearDeshabilitado = false;
+                
+                console.log(respuesta);
+            }
+            , function (error) {
+                $scope.botonCrearDeshabilitado = false;
+                console.log(error);
+            });
+
+    }
+
+    $scope.cargarUsuarios = function () {
+
+        $http({
+            method: "GET"
+            , url: "http://localhost:1337/Usuarios"
+            , isArray: true
+        }).then(
+            function (respuesta) {
+
+                $scope.usuarios = respuesta.data;
+
+                console.log(respuesta);
+            }
+            , function (error) {
+                console.log(error);
+            });
+
+
+
+    }
+
+    $scope.resetearNuevoUsuario = function () {
+        
+        $scope.nuevoUsuario = {
+            nombre: ""
+            , apellido: ""
+            , correo: ""
+            , password: ""
+            , fechaNac: ""
         }
-        , function (error) {
-            console.log(error);
-        });
+    }
+
+    $scope.cargarUsuarios();
 
 
-    //    $http({method: $scope.method, url: $scope.url, cache: $templateCache}).
-    //        then(function(response) {
-    //          $scope.status = response.status;
-    //          $scope.data = response.data;
-    //        }, function(response) {
-    //          $scope.data = response.data || "Request failed";
-    //          $scope.status = response.status;
-    //      });
-    //    
+
+
 
 
 });
