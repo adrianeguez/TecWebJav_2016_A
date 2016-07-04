c9b2c848ebd8b1f4b36fd82f8c86ecca65faf451
Adrian Eguez <eadepto@hotmail.com>
2016-07-04 15:01:16 -0500

    Desplegar Usuario y Chumas en Chumas

--- Codigo/Chumas/assets/Aplicacion/Vistas/ChumasCtrl.js
+++ Codigo/Chumas/assets/Aplicacion/Vistas/ChumasCtrl.js
@@ -2,6 +2,25 @@
 
 
 
-aplicacion.controller("ChumasCtrl",function($scope){
+aplicacion.controller("ChumasCtrl",function($scope,$stateParams,$http){
+    
+    $scope.cargarChumasDeUsuario = function(){
+        
+        $http({
+            method: "GET"
+            , url: "http://localhost:1337/Usuarios/"+$stateParams.idUsuario
+        }).then(
+            function (respuesta) {
+
+                $scope.usuario = respuesta.data;
+                $scope.chumas = respuesta.data.chumas;
+            }
+            , function (error) {
+                console.log(error);
+            });
+        
+    }
+    
+    $scope.cargarChumasDeUsuario();
         
 });
