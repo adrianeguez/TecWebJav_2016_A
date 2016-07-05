f4a5d2ffee05ad3032e9803aa8b0908ecb6603f2
Adrian Eguez <eadepto@hotmail.com>
2016-07-05 14:46:30 -0500

    Agregar Gastos

--- Codigo/Chumas/assets/Aplicacion/Vistas/GastosCtrl.js
+++ Codigo/Chumas/assets/Aplicacion/Vistas/GastosCtrl.js
@@ -3,6 +3,61 @@ aplicacion.controller("GastosCtrl", function (
     , $stateParams
     , $http) {
 
+    $scope.nuevoGasto = {
+        lugarCompra: ""
+        , precio: ""
+        , idChuma: $stateParams.idChuma
+        , idTrago: 0
+    }
+
+    $scope.crearTragosXChuma = function () {
+
+        $http({
+            method: 'POST'
+            , url: "http://localhost:1337/TragosXChuma"
+            , data: $scope.nuevoGasto
+        }).then(
+            function (respuesta) {
+
+                for (var i = 0; i < $scope.tragos.length; i++) {
+
+                    if ($scope.nuevoGasto.idTrago == $scope.tragos[i].id) {
+
+                        respuesta.data.idTrago = $scope.tragos[i];
+                    }
+
+                }
+                $scope.gastos.push(respuesta.data);
+
+                $scope.nuevoGasto = {
+                    lugarCompra: ""
+                    , precio: ""
+                    , idChuma: $stateParams.idChuma
+                    , idTrago: 0
+                }
+            }
+            , function (error) {
+                console.log(error);
+            })
+
+    }
+
+    $scope.cargarTragos = function () {
+
+        $http({
+            method: 'GET'
+            , isArray: true
+            , url: "http://localhost:1337/Tragos"
+        }).then(
+            function (respuesta) {
+                $scope.tragos = respuesta.data;
+            }
+            , function (error) {
+                console.log(error);
+
+            })
+    }
+
     $scope.cargarGastos = function () {
         $http({
             method: 'GET'
@@ -35,5 +90,6 @@ aplicacion.controller("GastosCtrl", function (
 
     $scope.cargarInformacionUsuarioChuma();
     $scope.cargarGastos();
+    $scope.cargarTragos();
 
 });
